5532860165eb1d0fb6d082434911e483a11910b0
JackDiD1 <bbagrationov@mail.ru>
2026-02-16 13:26:29 +0400

    fix display errors2

--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
-    path('products/', views.product_list, name='product_list')
+    path('', views.home, name='home'),
+    path('products/', views.product_list, name='product_list'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
     path('page/<slug:slug>/', views.page_detail, name='page_detail'),
 ]
